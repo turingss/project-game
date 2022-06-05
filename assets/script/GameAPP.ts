7af80c7e6e18d2e75c544f8319db128c7291e688
puyv <dhhefasllfji@qq.com>
2022-06-05 15:40:02 +0800

    init game...

---
+++ assets/script/GameAPP.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('GameAPP')
+export class GameAPP extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
