500f8c34ebbe135824f9bfca5e505d308626d0e4
puyv <dhhefasllfji@qq.com>
2022-06-05 16:59:08 +0800

    av1

--- assets/script/FeatureA.ts
+++ assets/script/FeatureA.ts
@@ -1,8 +1,26 @@
+/*
+ * @Description: 
+ * @Version: 1.0
+ * @Company: rct.AI
+ * @Autor: Yv Pu
+ * @Date: 2022-06-05 16:42:09
+ * @LastEditors: Yv Pu
+ * @LastEditTime: 2022-06-05 16:58:07
+ */
 import { _decorator, Component, Node } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('FeatureA')
 export class FeatureA extends Component {
+
+
+    public firstMethod(){
+        console.error("method1");
+    }
+
+
+    
+
     start() {
 
     }
--- assets/script/GameAPP.ts
+++ assets/script/GameAPP.ts
@@ -1,14 +1,26 @@
 import { _decorator, Component, Node } from 'cc';
+import { FeatureA } from './FeatureA';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameAPP')
 export class GameAPP extends Component {
-    start() {
 
+
+    private featurea:FeatureA = null;
+
+
+    start() {
+        
+        this.featurea = new FeatureA();
+        this.featurea.firstMethod();
+            
     }
 
     update(deltaTime: number) {
         
     }
+
+
+
 }
 
